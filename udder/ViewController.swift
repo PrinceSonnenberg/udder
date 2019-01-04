54c27939297e773af5f2745e4c80e19af475972f
Prince Sonnenberg <princesonnenberg@users-macbook-pro.local>
2019-01-04 05:48:45 +0200

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Prince Sonnenberg on 2019/01/04.
+//  Copyright © 2019 Prince Sonnenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
